55f6098d44199f9eb2af25eba7df3206b94c50c6
Animesh Bhadra <animeshb@archerimagine.com>
2019-06-23 16:57:45 +0530

    First practice question from https://devu.in/practice/

---
+++ code/practice/function.py
@@ -0,0 +1,14 @@
+# WAP to add two integers, floating point and complex numbers.
+def addition(x, y):
+    return x + y
+
+
+# Complex number addition.
+print(addition(1 + 2j, 3 + 4j))
+
+# Addition of negative and decimal number
+print(addition(-2, 1.5))
+
+# Normal positive number addition.
+print(addition(2, 4))
+
