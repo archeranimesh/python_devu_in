70044e85375042936b5c96bf4b78446791bf7f9a
Animesh Bhadra <animeshb@archerimagine.com>
2019-06-29 23:29:14 +0530

    basic file operation and properties.

--- code/Day_03/01_file.py
+++ code/Day_03/01_file.py
@@ -1,6 +1,6 @@
 import os
 
-file_name = r"pythonFile01.txt"
+file_name = r"/pythonFile01.txt"
 
 # https://stackoverflow.com/questions/1296501/find-path-to-currently-running-file
 def get_file_path():
@@ -9,3 +9,17 @@ def get_file_path():
 
 
 print(get_file_path())
+
+full_file_path = get_file_path() + file_name
+
+print(full_file_path)
+
+f = open(full_file_path, "w")
+print("type(f): ", type(f))
+print("f.mode: ", f.mode)
+print("f.name: ", f.name)
+print("f.readable(): ", f.readable())
+print("f.writable(): ", f.writable())
+print("f.closed: ", f.closed)
+
+f.close()
