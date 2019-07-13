f7f00b87982167f30eb653a8ea5f740ec24f0925
Animesh Bhadra <animeshb@archerimagine.com>
2019-07-13 18:12:41 +0530

    Basic Class

---
+++ code/Day_05/class_01.py
@@ -0,0 +1,11 @@
+class A:
+    def add(self, x, y):
+        return x + y
+
+    def sub(self, x, y):
+        return x - y
+
+
+if __name__ == "__main__":
+    objA = A()
+    print(objA.add(5, 6))
