3dde3a1c6552f3cc6b566331161f1e8900340be3
Animesh Bhadra <animeshb@archerimagine.com>
2019-07-14 16:41:45 +0530

    generator function

---
+++ code/Day_06/Class_01.py
@@ -0,0 +1,24 @@
+def generator_function(num):
+    i = 0
+    while i < num:
+        print("i = ", i)
+        i += 1
+
+
+# Original Generator
+def my_generator_function(num):
+    i = 0
+    while i < num:
+        yield i
+        i += 1
+
+
+if __name__ == "__main__":
+    print(generator_function(4))
+    x = my_generator_function(4)
+
+    try:
+        while True:
+            print(next(x))
+    except StopIteration as si:
+        print("iteration ended with: ")
